02b455667a48f90da5fb90c0384c5490252006ac
R15B3RG <143381712+R15B3RG@users.noreply.github.com>
2024-11-19 14:17:51 +0100

    Update

--- WebShopSolution/Repository/Model/Product.cs
+++ WebShopSolution/Repository/Model/Product.cs
@@ -5,5 +5,7 @@ namespace WebShop
     {
         public int Id { get; set; } // Unikt ID för produkten
         public string Name { get; set; } // Namn på produkten
+
+        public double Price { get; set; }
     }
 }
