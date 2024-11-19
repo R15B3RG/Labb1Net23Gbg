43fdb6d9ac84a61d400b7b626500da8680023126
R15B3RG <143381712+R15B3RG@users.noreply.github.com>
2024-11-19 14:24:31 +0100

    Update

--- WebShopSolution/Repository/Model/Customer.cs
+++ WebShopSolution/Repository/Model/Customer.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Repository.Model
 {
-    internal class Customer
+    public class Customer
     {
     }
 }
--- WebShopSolution/Repository/Repositories/Interfaces/IProductRepository.cs
+++ WebShopSolution/Repository/Repositories/Interfaces/IProductRepository.cs
@@ -1,12 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Repository.Repositories.Interfaces
+﻿namespace WebShop.Repositories
 {
+    // Gränssnitt för produktrepositoryt enligt Repository Pattern
     public interface IProductRepository
     {
+        IEnumerable<Product> GetAll(); // Hämtar alla produkter
+        void Add(Product product); // Lägger till en ny produkt
     }
 }
